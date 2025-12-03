beaeb3b49a2ef586d5880410a383cc087bfc5d6c
imlainwired <miamohsinshahzad@gmail.com>
2025-12-03 06:51:52 +0500

    Add header for priority non-preemptive scheduler

---
+++ priority_non_preemptive.hpp
@@ -0,0 +1,8 @@
+#ifndef PRIORITY_NON_PREEMPTIVE_HPP
+#define PRIORITY_NON_PREEMPTIVE_HPP
+#include "scheduler_common.hpp" 
+
+void run_priority_non_preemptive_simulation();
+void printSimulation_priority(const std::vector<Proc>& processes, int n, int current_time);
+
+#endif 
