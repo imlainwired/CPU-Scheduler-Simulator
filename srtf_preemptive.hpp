ed4239776fd7221c038d774459f0fb1db480c264
imlainwired <miamohsinshahzad@gmail.com>
2025-12-07 15:40:31 +0500

    Add SRTF preemptive simulation header file

---
+++ srtf_preemptive.hpp
@@ -0,0 +1,9 @@
+#ifndef SRTF_PREEMPTIVE_HPP
+#define SRTF_PREEMPTIVE_HPP
+
+#include "scheduler_common.hpp" 
+
+void run_srtf_preemptive_simulation();
+void printSimulation_srtf(const std::vector<Proc>& processes, int n, int current_time);
+
+#endif 
